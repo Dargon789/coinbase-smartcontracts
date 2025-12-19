b99b8a0b223bad398671f793de2e6aec60e49844
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2025-12-20 01:42:52 +0700

    coinbase-smartcontracts (#112)
    hardhat  foundry  toolchain ethereum test
    
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -14,7 +14,7 @@ export async function deployAll() {
 	);
 
 	const GreetingsRegistry = env.get<typeof env.artifacts.GreetingsRegistry.abi>('GreetingsRegistry');
-	const {deployer} = env.namedAccounts;
+	const deployer = env.namedAccounts.deployer;
 
 	return {env, GreetingsRegistry, deployer, otherAccounts: env.unnamedAccounts};
 }
