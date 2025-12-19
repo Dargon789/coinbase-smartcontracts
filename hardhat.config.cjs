28b6785280f5623b2e7c821ae055994c20d94054
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2025-12-20 01:40:29 +0700

    coinbase-smartcontracts
    hardhat  foundry  toolchain ethereum test
    
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,57 +1,7 @@
 const {loadEnv} = require('ldenv');
 loadEnv();
 require('@nomicfoundation/hardhat-network-helpers');
-// You can remove `hardhat-rocketh` and flatten your network setup by reading env vars directly:
-const { loadEnv } = require('ldenv');
-loadEnv();
-require('@nomicfoundation/hardhat-network-helpers');
-const { task } = require('hardhat/config');
-
-task('node').setAction(async (args, hre, runSuper) => {
-  if (process.env.BLOCK_TIME) {
-    hre.config.networks.hardhat.mining = {
-      auto: true,
-      interval: +process.env.BLOCK_TIME * 1000,
-    };
-  }
-  return runSuper(args);
-});
-
-const defaultVersion = '0.8.25';
-const defaultSettings = {
-  optimizer: { enabled: true, runs: 999999 },
-  viaIR: true,
-  outputSelection: { '*': { '*': ['evm.methodIdentifiers'] } },
-};
-
-// build custom networks from ETH_NODE_URI_<name> env vars
-const externalNetworks = Object.entries(process.env)
-  .filter(([key]) => key.startsWith('ETH_NODE_URI_'))
-  .reduce((acc, [key, url]) => {
-    const name = key.replace('ETH_NODE_URI_', '').toLowerCase();
-    acc[name] = { url };
-    return acc;
-  }, {});
-
-// configure hardhat forking and flags
-const hardhatNetwork = {
-  initialBaseFeePerGas: process.env.HARDHAT_FORK ? 1 : 0,
-  allowUnlimitedContractSize: !process.env.HARDHAT_FORK,
-  ...(process.env.HARDHAT_FORK && {
-    forking: { url: process.env.HARDHAT_FORK },
-  }),
-};
-
-module.exports = {
-  solidity: {
-    compilers: [{ version: defaultVersion, settings: { ...defaultSettings } }],
-  },
-  networks: {
-    hardhat: hardhatNetwork,
-    ...externalNetworks,
-  },
-  paths: { sources: 'src' },
-};
+const {addForkConfiguration, addNetworksFromEnv} = require('hardhat-rocketh');
 
 const {task} = require('hardhat/config');
 // we override the node task to set up our block time interval
@@ -95,7 +45,7 @@ const config = {
 			addNetworksFromEnv({
 				hardhat: {
 					initialBaseFeePerGas: process.env.HARDHAT_FORK ? 1 : 0,
-					allowUnlimitedContractSize: !process.env.HARDHAT_FORK,
+					allowUnlimitedContractSize: process.env.HARDHAT_FORK ? false : true,
 				},
 			}),
 		),
